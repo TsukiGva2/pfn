c4b2eef57ef5a2d3b2068e14286c03bc72190551
TsukiGva2 <dreamviolent56@gmail.com>
2021-06-14 19:49:47 -0300

    way better testing

--- pfn_test.go
+++ pfn_test.go
@@ -5,44 +5,28 @@ import (
 	"testing"
 )
 
-func TestAvg(t *testing.T) {
-	data, err := ioutil.ReadFile("test-expected/average.py")
+func makeTest(t *testing.T, filename string) {
+	data, err := ioutil.ReadFile("test-expected/" + filename + ".py")
 	if err != nil {
 		t.Error("error opening file")
 		return
 	}
 
-	out := runFile("pfn-code/average.pfn")
+	out := runFile("pfn-code/" + filename + ".pfn")
 
 	if string(data) != out {
 		t.Errorf("error, expected:\n%s\ngot:\n%s\n", string(data), out)
 	}
+}
+
+func TestAvg(t *testing.T) {
+	makeTest(t, "average")
 }
 
 func TestPM(t *testing.T) {
-	data, err := ioutil.ReadFile("test-expected/pattern.py")
-	if err != nil {
-		t.Error("error opening file")
-		return
-	}
-
-	out := runFile("pfn-code/pattern.pfn")
-
-	if string(data) != out {
-		t.Errorf("error, expected:\n%s\ngot:\n%s\n", string(data), out)
-	}
+	makeTest(t, "pattern")
 }
 
 func TestSimple(t *testing.T) {
-	data, err := ioutil.ReadFile("test-expected/simple.py")
-	if err != nil {
-		t.Error("error opening file")
-		return
-	}
-
-	out := runFile("pfn-code/simple.pfn")
-
-	if string(data) != out {
-		t.Errorf("error, expected:\n%s\ngot:\n%s\n", string(data), out)
-	}
+	makeTest(t, "simple")
 }
